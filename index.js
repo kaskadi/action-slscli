ca5f7b04a197a75259ae8a6851eca8fffa4e0748
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-23 16:37:51 +0100

    Logging cwd to check working directory in child process

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const childProc = require('child_process')
 const command = process.env.INPUT_COMMAND || ''
 
 childProc.exec(`./node_modules/.bin/sls ${command}`, (err, stdout, stderr) => {
+    console.log(process.cwd())
     console.log(stdout)
-    console.log(stderr)
     if (err !== null) {
-      throw err
+      console.log(stderr)
+      throw
     }
   })
