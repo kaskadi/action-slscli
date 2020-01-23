b708b5d3842a41c61fa7df3e6cfff272389138f5
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-23 16:18:48 +0100

    Implemented action + modules

---
+++ index.js
@@ -0,0 +1,12 @@
+const childProc = require('child_process')
+
+const command = process.env.INPUT_COMMAND || ''
+
+childProc.exec(`./node_modules/.bin/sls ${command}`, (error, stdout, stderr) => {
+    console.log(stdout)
+    console.log(stderr)
+    if (error !== null) {
+      console.log(`exec error: ${error}`)
+    }
+  })
+})
