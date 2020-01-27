43c4f6eaaac3fa0074edd4a729d4ba7c7281aa9b
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-27 13:28:52 +0100

    Fixed path to update script

--- update-action.sh
+++ update-action.sh
@@ -4,10 +4,10 @@ OUTDATED="$(npm outdated)"
 if [ -n "$OUTDATED" ]
 then
   npm update --dev
-  git config --global user.name 'Alexis Lemaire'
-  git config --global user.email 'a.lemaire@klimapartner.de'
-  git commit -am "Updated packages"
-  git push
+  # git config --global user.name 'Alexis Lemaire'
+  # git config --global user.email 'a.lemaire@klimapartner.de'
+  # git commit -am "Updated packages"
+  # git push
   echo "Packages have been updated"
 else
   echo "Packages are up to date, no proceeding to update."
