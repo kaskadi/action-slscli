644ac37699f571c566cb50321987363bb8da06a4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-29 16:17:19 +0100

    Using GitHub Actions runtime env variable to detect local/remote run

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const childProc = require('child_process')
 // test if we're in a GitHub Actions context so we can still test locally how the action is behaving
 
-const root = process.cwd().includes('/home/runner/work') ? '/home/runner/work/_actions/kaskadi/action-slscli/master/' : './'
+const root = process.env.GITHUB_ACTIONS ? '/home/runner/work/_actions/kaskadi/action-slscli/master/' : './'
 const pathToBin = `${root}node_modules/serverless/bin/serverless.js`
 
 const command = process.env.INPUT_COMMAND || ''
