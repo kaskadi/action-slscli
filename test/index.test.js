49d24440a5b55ec397e464d40959bc1f205b222c
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-26 12:21:44 +0100

    Aligned test logic with template + added timeout cap increase

--- test/index.test.js
+++ test/index.test.js
@@ -3,38 +3,38 @@ const childProc = require('child_process')
 const chai = require('chai')
 chai.should()
 
-describe('action-sls-cli', () => {
+describe('action-slscli', () => {
   it('should execute simple --version call', async () => {
-    const test = await testIndex({ command: '--version' })
+    const test = await execMain({ command: '--version' })
     test.should.equal(true)
-  })
+  }).timeout(4000)
   it('should process command when given a working directory', async () => {
-    const test = await testIndex({ command: 'deploy --noDeploy', wd: 'test/test-wd-sls' })
+    const test = await execMain({ command: 'deploy --noDeploy', wd: 'test/test-wd-sls' })
     test.should.equal(true)
-  })
+  }).timeout(4000)
   describe('processes valid files', () => {
     it('should process command', async () => {
-      const test = await testIndex({ command: 'deploy --noDeploy', wd: 'test/valid-sls', shouldThrow: 'false' })
+      const test = await execMain({ command: 'deploy --noDeploy', wd: 'test/valid-sls', shouldThrow: 'false' })
       test.should.equal(true)
-    })
+    }).timeout(4000)
     it('should not throw when enabling error catching', async () => {
-      const test = await testIndex({ command: 'deploy --noDeploy', wd: 'test/valid-sls', shouldThrow: 'true' })
+      const test = await execMain({ command: 'deploy --noDeploy', wd: 'test/valid-sls', shouldThrow: 'true' })
       test.should.equal(true)
-    })
+    }).timeout(4000)
   })
   describe('processes valid files', () => {
     it('should process command when not enabling error catching', async () => {
-      const test = await testIndex({ command: 'deploy --noDeploy', wd: 'test/invalid-sls', shouldThrow: 'false' })
+      const test = await execMain({ command: 'deploy --noDeploy', wd: 'test/invalid-sls', shouldThrow: 'false' })
       test.should.equal(true)
-    })
+    }).timeout(4000)
     it('should throw when enabling error catching', async () => {
-      const test = await testIndex({ command: 'deploy --noDeploy', wd: 'test/invalid-sls', shouldThrow: 'true' })
+      const test = await execMain({ command: 'deploy --noDeploy', wd: 'test/invalid-sls', shouldThrow: 'true' })
       test.should.equal(false)
-    })
+    }).timeout(4000)
   })
 })
 
-function testIndex (opts) {
+function execMain (opts) {
   return new Promise((resolve, reject) => {
     process.env.INPUT_COMMAND = opts.command
     if (opts.wd) {
