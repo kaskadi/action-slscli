df69dddff619438cd450ac7714a572ea5885a8e6
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-26 14:01:25 +0100

    Updated condition for root variable definition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const childProc = require('child_process')
 // test if we're in a GitHub Actions context so we can still test locally how the action is behaving
 
-const root = process.env.GITHUB_ACTIONS ? '/home/runner/work/_actions/kaskadi/action-slscli/master/' : `${process.cwd()}/`
+const root = process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY !== 'kaskadi/action-slscli' ? '/home/runner/work/_actions/kaskadi/action-slscli/master/' : `${process.cwd()}/`
 const pathToBin = `${root}node_modules/serverless/bin/serverless.js`
 
 const command = process.env.INPUT_COMMAND || ''
