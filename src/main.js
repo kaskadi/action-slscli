cce3d50b0b04485108f95cc5c93a4d29e7945d03
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-03 18:43:48 +0100

    Added log grouping

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
 const { spawn } = require('child_process')
 const path = require('path')
-const core = require('@actions/core')
+const { getInput, group } = require('@actions/core')
 
 async function main () {
   const pathToBin = path.join(__dirname, '..', 'node_modules/serverless/bin/serverless.js')
-  const command = core.getInput('command')
-  const wd = core.getInput('working_directory')
-
-  if (wd.length > 0) {
-    process.chdir(wd)
-  }
-
-  let throwControl = core.getInput('should_throw')
+  const command = getInput('command')
+  const wd = getInput('working_directory')
+  let throwControl = getInput('should_throw')
   throwControl = throwControl.length > 0 ? JSON.parse(throwControl) : false
 
-  await new Promise((resolve, reject) => {
-    const proc = spawn(pathToBin, command.split(' '))
-    proc.stdout.on('data', log)
-    proc.stderr.on('data', log)
-    proc.on('error', console.log)
-    proc.on('exit', code => {
-      if (code !== 0 && throwControl) {
-        reject(new Error(code))
-      }
-      resolve(code)
+  if (wd.length > 0) {
+    console.log(`INFO: changing working directory to ${wd}...`)
+    process.chdir(wd)
+    console.log('SUCCESS: successfully changed working directory!')
+  }
+
+  await group(`Executing command ${command} in Serverless CLI`, async () => {
+    return new Promise((resolve, reject) => {
+      const proc = spawn(pathToBin, command.split(' '))
+      proc.stdout.on('data', log)
+      proc.stderr.on('data', log)
+      proc.on('error', console.log)
+      proc.on('exit', code => {
+        if (code !== 0 && throwControl) {
+          reject(new Error(code))
+        }
+        resolve(code)
+      })
     })
   })
 }
